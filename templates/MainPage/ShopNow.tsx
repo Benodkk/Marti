f961e64e3b4a9789ed17a2f19e2b1c5561b2efa9
Daniel Kozlowski <daniel.kozlowski2607@gmail.com>
2024-01-05 15:21:50 +0100

    update

--- templates/MainPage/ShopNow.tsx
+++ templates/MainPage/ShopNow.tsx
@@ -4,32 +4,9 @@ import { OneShopNow } from "./components/OneShopNow";
 import ForMen from "@/assets/ForMen.png";
 import ForWomen from "@/assets/ForWomen.png";
 
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-
 interface ShopNowProps {}
 
 export const ShopNow = ({}: ShopNowProps) => {
-  const bebe = async () => {
-    const api = new WooCommerceRestApi({
-      url: "http://panel.martibikini.com",
-      consumerKey: "ck_0d62184ebd8b521eee92dc372b9dea9fb1461fe4",
-      consumerSecret: "cs_1caae09f86e2c00f474c08cf1f262ceadeb307ce",
-      version: "wc/v3",
-    });
-    api
-      .get("products")
-      .then((response: any) => {
-        // Log the response data to the console
-        console.log(response.data);
-      })
-      .catch((error: any) => {
-        // Log the error response data to the console
-        console.log(error);
-      });
-  };
-  useEffect(() => {
-    bebe();
-  }, []);
   return (
     <StyledShopNowContainer>
       <OneShopNow backgroundColor="#C44370" imageSrc={ForMen.src} label="Men" />
--- templates/ProductList/ProductList.tsx
+++ templates/ProductList/ProductList.tsx
@@ -9,35 +9,9 @@ import { ListFilters } from "./ListFilters";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-
 interface ProductListProps {}
 
 export const ProductList = ({}: ProductListProps) => {
-  const [testState, setTestState] = useState<any>("");
-  const bebe = async () => {
-    const api = new WooCommerceRestApi({
-      url: "http://panel.martibikini.com",
-      consumerKey: "ck_0d62184ebd8b521eee92dc372b9dea9fb1461fe4",
-      consumerSecret: "cs_1caae09f86e2c00f474c08cf1f262ceadeb307ce",
-      version: "wc/v3",
-    });
-    api
-      .get("products")
-      .then((response: any) => {
-        // Log the response data to the console
-        console.log(response.data);
-      })
-      .catch((error: any) => {
-        // Log the error response data to the console
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    bebe();
-  }, []);
-
   return (
     <ProductListContainer>
       <ProductListHeader
